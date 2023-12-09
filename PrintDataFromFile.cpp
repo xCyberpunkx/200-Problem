161984064e86954a42dc1505d955901598f350f6
xCyberpunkx <116972275+xCyberpunkx@users.noreply.github.com>
2023-12-09 17:16:58 +0100

    Fixed Logical Error

--- PrintDataFromFile.cpp
+++ PrintDataFromFile.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <iostream>
 #include <cstdio>
 #include <vector>
 #include <fstream>
@@ -14,7 +15,6 @@ const string ClientFileName = "Clients.txt";
 struct stClients
 {
     string ClientName;
-    string AccountName;
     string AccountNumber;
     string PINCODE;
     string Phone;
@@ -24,7 +24,7 @@ static vector<string> SplitString(string Str, string Delimeter)
 {
     vector<string> vString;
     short pos = 0;
-    string word; 
+    string word;
     // use the find() func to find the position of the delimeter
     while ((pos = Str.find(Delimeter)) != std::string::npos)
     {
@@ -41,21 +41,26 @@ static vector<string> SplitString(string Str, string Delimeter)
     }
     return vString;
 }
-static stClients ConvertRecordToLine(string Line, string seperator = "|||")
-{
-
+static stClients ConvertRecordToLine(string Line, string separator = "|||") {
     stClients Client;
     vector<string> vClientData;
-    vClientData = SplitString(Line, seperator);
-    Client.ClientName = vClientData[0];
-    Client.AccountName = vClientData[1];
-    Client.AccountNumber = vClientData[2];
-    Client.PINCODE = vClientData[3];
-    Client.Phone = vClientData[4];
-    Client.AccountBalance = stod(vClientData[5]);
+    vClientData = SplitString(Line, separator);
+
+    if (vClientData.size() <= 6) { // Ensure there are at least 6 elements in the vector
+        Client.ClientName = vClientData[0];
+        Client.AccountNumber = vClientData[1];
+        Client.PINCODE = vClientData[2];
+        Client.Phone = vClientData[3];
+        Client.AccountBalance = stod(vClientData[4]);
+    }
+    else {
+        // Handle the case where the data doesn't have enough elements
+        // For example:
+        cerr << "Incomplete data in the record: " << Line << endl;
+        // You might want to set default values for Client or handle the error accordingly
+    }
 
     return Client;
-
 }
 static vector<stClients> LoadClientsDataFromFile(string FileName)
 {
@@ -66,8 +71,8 @@ static vector<stClients> LoadClientsDataFromFile(string FileName)
     if (MyFile.is_open())
     {
         string line;
-       stClients Client;
-        while (getline(MyFile,line))
+        stClients Client;
+        while (getline(MyFile, line))
         {
             Client = ConvertRecordToLine(line);
             vClient.push_back(Client);
@@ -79,23 +84,22 @@ static vector<stClients> LoadClientsDataFromFile(string FileName)
 }
 static void PrintClientRecord(stClients Client)
 {
-    std::cout << "| " << setw(15) << left << Client.ClientName;
-    std::cout << "| " << setw(10) << left << Client.AccountName;
-    std::cout << "| " << setw(30) << left << Client.AccountNumber;
+    std::cout << "| " << setw(20) << left << Client.AccountNumber;
     std::cout << "| " << setw(20) << left << Client.PINCODE;
-    std::cout << "| " << setw(15) << left << Client.Phone;
-    std::cout << "| " << setw(15) << left << Client.AccountBalance;
+    std::cout << "| " << setw(20) << left << Client.ClientName;
+    std::cout << "| " << setw(20) << left << Client.Phone;
+    std::cout << "| " << setw(20) << left << Client.AccountBalance;
 }
 static void PrintAllClientsRecord(vector<stClients> vClients)
 {
     std::cout << "\n\t\t\t\tClient List (" << vClients.size() << ")Clients(s)";
     std::cout << "\n_______________________________________________________";
     std::cout << "_______________________________________________________\n";
-    std::cout << "| " << left << setw(15) << "Accout Number";
-    std::cout << "| " << left << setw(10) << "Pin Code";  
-    std::cout << "| " << left << setw(40) << "Client Name";
-    std::cout << "| " << left << setw(12) << "Phone";
-    std::cout << "| " << left << setw(12) << "Balance";
+    std::cout << "| " << left << setw(20) << "Client Name";
+    std::cout << "| " << left << setw(20) << "Pin Code";
+    std::cout << "| " << left << setw(20) << "Accout Number"; 
+    std::cout << "| " << left << setw(20) << "Phone";
+    std::cout << "| " << left << setw(20) << "Balance";
     std::cout << "\n_______________________________________________________";
     std::cout << "_______________________________________________________\n";
 
@@ -110,13 +114,12 @@ static void PrintAllClientsRecord(vector<stClients> vClients)
 
 int main()
 {
-    vector<stClients> vClient =  LoadClientsDataFromFile(ClientFileName);
+    vector<stClients> vClient = LoadClientsDataFromFile(ClientFileName);
     PrintAllClientsRecord(vClient);
-   
+
     system("pause>0");
     return 0;
 }
-
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
